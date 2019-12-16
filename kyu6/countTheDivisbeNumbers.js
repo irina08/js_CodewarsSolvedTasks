fc2ca5123b1b62ae781a0766ea8a9fdfb5da5278
irina08 <irina_ger@yahoo.com>
2019-12-15 18:01:34 -0800

    Added countTheDivisibleNumbers.js

---
+++ kyu6/countTheDivisbeNumbers.js
@@ -0,0 +1,14 @@
+/*
+Complete the function that takes 3 numbers x, y and k
+(where x ≤ y), and returns the number of integers within
+the range [x..y] (both ends included) that are divisible by k.
+More scientifically: { i : x ≤ i ≤ y, i mod k = 0 }
+Example
+Given x = 6, y = 11, k = 2 the function should return 3, because
+there are three numbers divisible by 2 between 6 and 11: 6, 8, 10
+ */
+
+function divisibleCount(x, y, k) {
+    if (x % k === 0) return (Math.floor(y / k) - Math.floor(x / k)) + 1;
+    else return Math.floor(y / k) - Math.floor(x / k);
+}
