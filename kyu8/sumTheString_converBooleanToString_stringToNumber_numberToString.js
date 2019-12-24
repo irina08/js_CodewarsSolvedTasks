6ea9a23ccd0d14298ed6107bc2332c22de3b5132
irina08 <irina_ger@yahoo.com>
2019-12-23 22:35:24 -0800

    Added sumTheString_converBooleanToString_stringToNumber_numberToString.js

---
+++ kyu8/sumTheString_converBooleanToString_stringToNumber_numberToString.js
@@ -0,0 +1,16 @@
+function sumStr(a,b) {
+    return String(Number(a) + Number(b));
+}
+
+const booleanToString = b =>
+    b.toString();
+
+var stringToNumber = function(str){
+    return Number(str);
+}
+
+var a = (123).toString();
+
+function numberToString(num) {
+    return String(num);
+}
