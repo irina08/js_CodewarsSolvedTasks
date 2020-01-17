e4ede0c7e3b5ca5f5815891661bc9b412bade9da
irina08 <irina_ger@yahoo.com>
2020-01-16 23:04:50 -0800

    Added aStringOfSort.js.

---
+++ kyu6/aStringOfSorts.js
@@ -0,0 +1,21 @@
+/* Define a method that accepts 2 strings as parameters.
+The method returns the first string sorted by the second.
+sortString("foos", "of")       => "oofs"
+sortString("string", "gnirts") => "gnirts"
+sortString("banana", "abn")    => "aaabnn"
+To elaborate, the second string defines the ordering. It is possible
+that in the second string characters repeat, so you should remove
+repeating characters, leaving only the first occurrence.
+Any character in the first string that does not appear in the second
+string should be sorted to the end of the result in original order.
+ */
+
+function sortString(string,ordering) {
+    ordering = ordering.split('');
+    let array1 = string
+        .split('')
+        .filter(char => ordering.includes(char))
+        .sort((a, b) => ordering.indexOf(a) - ordering.indexOf(b));
+    let array2 = string.split('').filter(char => !ordering.includes(char));
+    return array1.join('') + array2.join('');
+}
