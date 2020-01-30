ba711340c04fdadb8c9b7a35212f136a33574888
irina08 <irina_ger@yahoo.com>
2020-01-29 21:09:44 -0800

    added goose.js.

---
+++ kyu8/goose.js
@@ -0,0 +1,16 @@
+/* Given an array of Player objects and an index (1-based),
+return the name of the chosen Player(name is a property of
+Player objects, e.g Player.name)
+Example:
+duck_duck_goose([a, b, c, d], 1) should return a.name
+duck_duck_goose([a, b, c, d], 5) should return a.name
+duck_duck_goose([a, b, c, d], 4) should return d.name
+*/
+function duckDuckGoose(players, goose) {
+    return players[(goose - 1) % players.length].name;
+
+    /* while (players.length < goose) {
+    goose = goose - players.length
+    }
+    return players[goose - 1].name */
+}
