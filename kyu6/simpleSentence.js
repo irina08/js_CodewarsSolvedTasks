d7b1f99e7de1d6d14be8a1dc27b660ed5e3a2dd2
irina08 <irina_ger@yahoo.com>
2020-03-24 00:24:34 -0700

    added simpleSentence.js.

---
+++ kyu6/simpleSentence.js
@@ -0,0 +1,19 @@
+/* Array of parts could contain:
+words;
+commas in the middle;
+multiple periods at the end.
+
+Sentence making rules:
+there must always be a space between words;
+there must not be a space between a comma and word on the left;
+there must always be one and only one period at the end of a sentence.
+
+Example:
+makeSentence(['hello', ',', 'my', 'dear']) // returns 'hello, my dear.'
+ */
+
+function makeSentence(parts) {
+  let str = '';
+  str = parts.join(' ').replace(/\s,/g, ',').replace(/\./g, '').trim();
+  return str + '.';
+}
