592f6f04caf370dc2dd7e92a6a7de4fd4e4d8f2c
irina08 <irina_ger@yahoo.com>
2020-03-30 00:13:06 -0700

    changed commonArrayElements.js.

--- kyu6/commonArrayElements.js
+++ kyu6/commonArrayElements.js
@@ -24,3 +24,16 @@ function common(a,b,c){
   }
   return sum;
 }
+
+function common(a,b,c){
+  let array = [];
+  for (let i = 0; i < a.length; i++) {
+    let temp = a[i];
+    if (b.includes(temp) && c.includes(temp)) {
+      b.splice(b.indexOf(temp), 1);
+      c.splice(c.indexOf(temp), 1);
+      array.push(temp);
+    }
+  }
+  return array.reduce((x, y) => x + y, 0);
+}
