89183986c70aed44c530bc70bdd2126b49086d8b
irina08 <irina_ger@yahoo.com>
2020-04-06 17:50:24 -0700

    added uniqueInOrder.js.

---
+++ kyu6/uniqueInOrder.js
@@ -0,0 +1,21 @@
+/* Implement the function unique_in_order which takes as argument
+a sequence and returns a list of items without any elements with
+the same value next to each other and preserving the original order of elements.
+
+For example:
+uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+ */
+
+let uniqueInOrder=function(iterable){
+  let arr = [];
+  let result = [];
+  if (Array.isArray(iterable) === true) arr = iterable;
+  else arr = iterable.split('');
+
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] !== arr[i-1]) result.push(arr[i]);
+  }
+  return result;
+}
