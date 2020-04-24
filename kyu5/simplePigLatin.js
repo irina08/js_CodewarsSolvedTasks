61a59b22bbbbecf0ce1e72f17774c82be4ab9bbb
irina08 <irina_ger@yahoo.com>
2020-04-24 11:36:55 -0700

    added  second solution for simplePigLatin.js

--- kyu5/simplePigLatin.js
+++ kyu5/simplePigLatin.js
@@ -4,6 +4,23 @@ Examples
 pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
 pigIt('Hello world !');     // elloHay orldway !
  */
+
+function pigIt(str) {
+  let result = [];
+  let array = str.split(" ");
+  for (let i = 0; i < array.length; i++) {
+    if (array[i]!='?' && array[i]!='!') {
+      let word = array[i];
+      let letter = word.charAt(0);
+      let newWord = word.slice(1) + letter + "ay";
+      result.push(newWord);
+    } else {
+      result.push(array[i]);
+    }
+  }
+  return result.join(" ");
+}
+
 function pigIt(str) {
   str = str.trim().split(/\s{1,}/);
   return str.map(val => {
