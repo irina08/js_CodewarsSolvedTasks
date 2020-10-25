a1c6b2a7891c6c06a21ce964ee332a8a8930e50c
irina08 <irina_ger@yahoo.com>
2020-10-25 01:13:37 -0700

    added sumEvenNumbers.js

---
+++ kyu7/sumEvenNumbers.js
@@ -0,0 +1,14 @@
+/* Write a function named sumEvenNumbers, taking a sequence of numbers as single parameter.
+Your function must return the sum of the even values of this sequence.
+Only numbers without decimals like 4 or 4.0 can be even.
+Input
+sequence of numbers: those numbers could be integers and/or floats.
+For example, considering this input value : [4,3,1,2,5,10,6,7,9,8], then your function
+should return 30 (because 4 + 2 + 10 + 6 + 8 = 30).
+ */
+
+function sumEvenNumbers(input) {
+  let arr = input.filter(el => el % 2 === 0);
+  let arrSum = arr.reduce((a,b) => a + b, 0);
+  return arrSum;
+}
