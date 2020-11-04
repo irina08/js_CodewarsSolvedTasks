594921f02d1cbe70cb334cc5d09771eefcba8271
irina08 <irina_ger@yahoo.com>
2020-11-04 13:56:40 -0800

    added initializeMyName.js

---
+++ kyu7/initializeMyName.js
@@ -0,0 +1,19 @@
+/* You task is to initialize the middle names (if there is any).
+
+Examples
+'Jack Ryan'                   => 'Jack Ryan'
+'Lois Mary Lane'              => 'Lois M. Lane'
+'Dimitri'                     => 'Dimitri'
+'Alice Betty Catherine Davis' => 'Alice B. C. Davis'
+
+ */
+
+function initializeNames(name){
+  name = name.split(' ')
+  if (name.length > 2) {
+    for (let i = 1; i < name.length-1; i++)
+      name[i]= name[i][0]+'.'
+  }
+  return name.join(' ')
+}
+
