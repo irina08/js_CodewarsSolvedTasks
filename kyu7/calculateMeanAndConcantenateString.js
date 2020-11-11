8a06bae62501d719b320e27157f724ec4df6476e
irina08 <irina_ger@yahoo.com>
2020-11-11 00:06:01 -0800

    added characterCounter.js

--- kyu7/calculateMeanAndConcantenateString.js
+++ kyu7/calculateMeanAndConcantenateString.js
@@ -17,3 +17,17 @@ function mean(lst){
   ];
 }
 
+/*
+function mean(lst){
+  let num = 0, str = '';
+  lst.forEach(function(n) {
+    if(!isNaN(n)) {
+      num += +n;
+    }
+    else {
+      str += n;
+    }
+  });
+  return [num / 10, str];
+}
+ */
