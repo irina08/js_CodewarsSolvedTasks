8ad3078eaa2e7e63c6ee95ede661dee60fe00e62
irina08 <irina_ger@yahoo.com>
2020-11-17 11:08:57 -0800

    added countTheNumberOfJavaScriptDevelopersComingFromEurope.js

---
+++ kyu7/countTheNumberOfJavaScriptDevelopersComingFromEurope.js
@@ -0,0 +1,31 @@
+/* You will be given an array of objects (hashes in ruby) representing
+data about developers who have signed up to attend the coding meet up
+that you are organising for the first time.
+
+Your task is to return the number of JavaScript developers coming from Europe.
+
+For example, given the following list:
+
+var list1 = [
+  { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+  { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+  { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+  { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+];
+
+ */
+
+function countDevelopers(list) {
+  let count = 0;
+  for(let i = 0; i < list.length; i++) {
+    if(list[i].language === 'JavaScript' && list[i].continent === 'Europe')
+      count++;
+  }
+  return count;
+}
+
+/*
+function countDevelopers(list) {
+  return list.filter(x => x.continent === 'Europe' && x.language === 'JavaScript').length;
+}
+ */
