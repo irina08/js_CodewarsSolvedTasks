8751271749fd807780bc7bf28c06ef4be02d074c
irina08 <irina_ger@yahoo.com>
2020-11-17 11:07:56 -0800

    added second solution to countTheNumberOfExclamationMarksAndQuestionMarks.js

--- kyu7/countTheNumberOfExclamationMarksAndQuestionMarks.js
+++ kyu7/countTheNumberOfExclamationMarksAndQuestionMarks.js
@@ -17,3 +17,24 @@ function product(s){
   return (s.match(/\?/g) || []).length * (s.match(/!/g) || []).length;
 }
 
+/*
+function product(s){
+  let ques = 0;
+  let excl = 0;
+
+    s.split('').forEach(function(item){
+      if (item === "!"){
+        excl += 1;
+      }
+      if(item === "?"){
+        ques += 1;
+      }
+      if(item === ""){
+        return 0;
+      }
+    });
+
+  return excl * ques;
+}
+ */
+
