a5b26a29f0d3068ea2a7c9ed5ee7a47006f64ee0
irina08 <irina_ger@yahoo.com>
2020-11-23 00:03:09 -0800

    added filterTheGeese.js

---
+++ kyu8/filterTheGeese.js
@@ -0,0 +1,20 @@
+/* Write a function, gooseFilter / goose-filter / goose_filter /GooseFilter,
+that takes an array of strings as an argument and returns a filtered array
+containing the same elements but with the 'geese' removed.
+The geese are any strings in the following array, which is pre-populated
+in your solution:
+geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+
+For example, if this array were passed as an argument:
+["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]
+
+Your function would return the following array:
+["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
+ */
+
+function gooseFilter (birds) {
+  let geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  return birds.filter((el) => !geese.includes(el));
+
+};
+
