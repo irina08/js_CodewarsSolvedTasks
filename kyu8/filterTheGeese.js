5fedc521d92504474e8fcb1c2fe9858ac730c978
irina08 <irina_ger@yahoo.com>
2020-11-24 01:10:03 -0800

    added second solution to filterTheGeese.js

--- kyu8/filterTheGeese.js
+++ kyu8/filterTheGeese.js
@@ -18,3 +18,16 @@ function gooseFilter (birds) {
 
 };
 
+//second solution
+/*
+function gooseFilter (birds) {
+  let geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  let newArray = [];
+  for (let i =0; i<birds.length; i++) {
+    if (!geese.includes(birds[i])) {
+     newArray.push(birds[i]);
+    }
+  }
+  return newArray;
+};
+ */
