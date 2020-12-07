e3d45c9c3ec157fd461a39443afc6c0cc5b395d8
irina08 <irina_ger@yahoo.com>
2020-12-07 01:11:17 -0800

    added second solution to greetDevelopers.js

--- kyu7/greetDevelopers.js
+++ kyu7/greetDevelopers.js
@@ -45,3 +45,12 @@ function greetDevelopers(list) {
   return list;
 }
 
+/*
+function greetDevelopers(list) {
+  list.forEach(function(developer) {
+    developer.greeting = `Hi ${developer.firstName}, what do you like the most about ${developer.language}?`;
+  });
+
+  return list;
+}
+ */
