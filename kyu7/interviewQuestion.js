4c768225525240796099b9a29f7334bc4e4e9333
irina08 <irina_ger@yahoo.com>
2020-12-08 00:12:54 -0800

    added interviewQuestion.js

---
+++ kyu7/interviewQuestion.js
@@ -0,0 +1,45 @@
+/* You receive the name of a city as a string,
+and you need to return a string that shows how many
+times each letter shows up in the string by using an asterisk (*).
+
+For example:
+
+"Chicago"  -->  "c:**,h:*,i:*,a:*,g:*,o:*"
+As you can see, the letter c is shown only once, but with 2 asterisks.
+
+The return string should include only the letters
+(not the dashes, spaces, apostrophes, etc). There should be
+no spaces in the output, and the different letters are separated
+by a comma (,) as seen in the example above.
+
+Note that the return string must list the letters in order of
+their first appearence in the original string.
+
+More examples:
+
+"Bangkok"    -->  "b:*,a:*,n:*,g:*,k:**,o:*"
+"Las Vegas"  -->  "l:*,a:**,s:**,v:*,e:*,g:*"
+
+ */
+
+function getStrings(city) {
+  city = city.toLowerCase();
+  let obj = {};
+  let str = '';
+
+  for (let elem of city) {
+    if (!(elem in obj)) {
+      obj[elem] = '*';
+    } else {
+      obj[elem] += '*';
+    }
+  }
+
+  for (let key in obj) {
+    if (key !== ' ') {
+      str += key + ':' + obj[key] + ',';
+    }
+  }
+
+  return str.substring(0, str.length - 1);
+}
