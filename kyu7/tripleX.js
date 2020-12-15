eee427c169ad4137be5bb42f26e21b27246766ee
irina08 <irina_ger@yahoo.com>
2020-12-15 00:34:56 -0800

    added tripleX.js

---
+++ kyu7/tripleX.js
@@ -0,0 +1,21 @@
+/*
+Given a string, return true if the first instance of "x" in the string
+is immediately followed by the string "xx".
+
+tripleX("abraxxxas") → true
+tripleX("xoxotrololololololoxxx") → false
+tripleX("softX kitty, warm kitty, xxxxx") → true
+tripleX("softx kitty, warm kitty, xxxxx") → false
+Note :
+
+capital X's do not count as an occurrence of "x".
+if there are no "x"'s then return false
+ */
+
+function tripleX(str){
+  console.log(str)
+  if(str[str.indexOf('x') + 1] === 'x' && str[str.indexOf('x') + 2] === 'x')
+    return true;
+  else
+    return false;
+}
